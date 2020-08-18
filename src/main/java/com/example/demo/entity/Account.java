27c384bbef3a15e43c5e29cc43415a08e773fc65
k152744 <yasukiyasuki1@icloud.com>
2020-08-18 22:04:32 +0900

    first

--- src/main/java/com/example/demo/entity/Account.java
+++ src/main/java/com/example/demo/entity/Account.java
@@ -1,10 +1,35 @@
 package com.example.demo.entity;
 
-import lombok.Data;
+// import lombok.Data;
 
-@Data
+
 public class Account{
   private int id;
   private String name;
   private String password;
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
 }
